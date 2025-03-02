9c90751bb6faab32a2e814aa86a35bdc6792139c
NadezhdaTA <nadezda.tupitsyna@yandex.ru>
2025-03-02 11:51:04 +0400

    Финальное задание 10-го спринта с доработками

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends Exception {
 
-    public ValidationException () {
+    public ValidationException() {
         super();
     }
 
